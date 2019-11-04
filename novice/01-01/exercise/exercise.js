5ea495e00413873b7fa66a9a75c6f8be7f6b12af
HasymiMuhamad <hasymimuhamad@gmail.com>
2019-11-05 06:24:38 +0700

    commit

---
+++ novice/01-01/exercise/exercise.js
@@ -0,0 +1,14 @@
+function setengahSegitiga (Tingginya) {
+
+    for (let a = 1; a <= Tingginya; a++) {
+      let tampilkan = '';
+  
+      for (let b = 1; b <= a; b++) {
+        tampilkan += '#';
+      }
+  
+      console.log(tampilkan);
+    }
+  }
+  
+  setengahSegitiga(7);
