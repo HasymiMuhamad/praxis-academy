4032ba16741451fe5ba16e2727d21a2ebfb70f8b
HasymiMuhamad <hasymimuhamad@gmail.com>
2019-11-05 07:34:32 +0700

    commit

--- novice/01-01/exercise/chessboard.js
+++ novice/01-01/exercise/chessboard.js
@@ -1,13 +1,12 @@
 function papanCatur(){
     let caturPapan= '', ukuran=8, a;
     
-
-for(let b = 0 ; b < ukuran ; b++) {
-  a= b%2 ? '# ' : ' #';
-  for(let c = 0 ; c < ukuran/2 ; c++) {
-    caturPapan+= a;
-  }
-  caturPapan+= '\n';
+for (let b = 0; b < ukuran ; b++){
+    a=b%2 ? '# ' : ' #';
+    for (let c = 0; c < ukuran/2; c++){
+        caturPapan+= a;
+    }
+    caturPapan+= '\n'
 }
 
 console.log(caturPapan);
