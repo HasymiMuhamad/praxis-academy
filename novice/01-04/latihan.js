c5b4c994982738797fec2a96d19e8d08cb14e1e8
HasymiMuhamad <hasymimuhamad@gmail.com>
2019-11-07 16:49:28 +0700

    commit

---
+++ novice/01-04/latihan.js
@@ -0,0 +1,16 @@
+function belajarString (){
+    const coba = new String('latihan');
+    console.log(coba);
+    typeof coba;
+}
+
+belajarString();
+
+function belajar2(){
+    const halo = "Semangat Pagi"
+    const panjangHuruf = halo.length;
+    console.log(halo[0]);
+    console.log(panjangHuruf);
+}
+
+belajar2();
